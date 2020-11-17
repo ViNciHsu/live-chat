07a8be2ecbc67e05eec87cffee3291227db01385
ViNciHsu <ViNciHsu>
2020-11-17 20:38:40 +0800

    modify <p> in communicationmessage

--- resources/views/communicationmessages/index.blade.php
+++ resources/views/communicationmessages/index.blade.php
@@ -4,8 +4,8 @@
         @foreach($communicationmessages as $communicationmessage)
             <li class="communicationmessage clearfix">
                 <div class="{{ ($communicationmessage->sender == Auth::id()) ? 'messagesent' : 'messagereceived'}}">
-                    <p>{{ $communicationmessage->communicationmessage }}</p>
-                    <p class="messagedate">{{ date('d M y, h:i a', strtotime($communicationmessage->created_at)) }}</p>
+                    <p class="word">{{ $communicationmessage->communicationmessage }}</p>
+                    <p class="messagedata">{{ date('d M y, h:i a', strtotime($communicationmessage->created_at)) }}</p>
                 </div>
             </li>
         @endforeach
